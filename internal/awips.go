499e96cdaff99b8e58e88fc372a675bdfb69eaff
Ryan Adam <ryan.z.adam@gmail.com>
2024-01-09 00:49:23 +1000

    fixed order of operations

--- internal/awips.go
+++ internal/awips.go
@@ -14,11 +14,10 @@ type AWIPS struct {
 func ParseAWIPS(text string) (AWIPS, error) {
 	awipsRegex := regexp.MustCompile("(?m:^[A-Z0-9 ]{6}\n)")
 	original := awipsRegex.FindString(text)
-	original = original[:len(original)-1]
-
 	if original == "" {
 		return AWIPS{}, errors.New("could not find AWIPS ID")
 	}
+	original = original[:len(original)-1]
 
 	product := original[0:3]
 	wfo := original[3:6]
