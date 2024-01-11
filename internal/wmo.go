f01a164ff5fc28806dc2df358b346e60601cff7c
Ryan Adam <ryan.z.adam@gmail.com>
2024-01-11 20:35:12 +1300

    added support for bbb field

--- internal/wmo.go
+++ internal/wmo.go
@@ -12,10 +12,11 @@ type WMO struct {
 	Datatype string    `json:"datatype"`
 	WFO      string    `json:"wfo"`
 	Issued   time.Time `json:"issued"`
+	BBB      string    `json:"bbb,omitempty"`
 }
 
 func ParseWMO(text string, issuedT time.Time) (WMO, error) {
-	wmoRegexp := regexp.MustCompile(`([0-9A-Z]{6,6})\s([A-Z]{4,4})\s([0-9]{6,6})`)
+	wmoRegexp := regexp.MustCompile(`([0-9A-Z]{6})\s([A-Z]{4})\s([0-9]{6})( [A-Z]{3})?`)
 	original := wmoRegexp.FindString(text)
 	if original == "" {
 		return WMO{}, errors.New("could not find WMO line")
@@ -41,10 +42,16 @@ func ParseWMO(text string, issuedT time.Time) (WMO, error) {
 		return WMO{}, errors.New("could not find WMO issued datetime")
 	}
 
+	bbb := ""
+	if len(segments) > 3 {
+		bbb = segments[3]
+	}
+
 	return WMO{
 		Original: original,
 		Datatype: segments[0],
 		WFO:      segments[1],
 		Issued:   issued,
+		BBB:      bbb,
 	}, nil
 }
